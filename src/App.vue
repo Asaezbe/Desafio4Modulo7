7e2100fe58353fa7dc1e717dbdb398d43ef73264
Asaezbe <asaezbe@gmail.com>
2024-09-26 16:15:03 -0300

    repositorioPrimerComit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div id="app">
+    <h1>Lista de usuarios</h1>
+
+    
+    <datosDeEntrada />
+    <tablaCompo />
+
+  </div>
+</template>
+
+<script>
+
+
+import datosDeEntrada from './components/datosDeEntrada.vue';
+import tablaCompo from './components/tablaCompo.vue' ;
+
+export default {
+  components: {
+    tablaCompo,
+    datosDeEntrada,
+  },
+};
+</script>
+
+
